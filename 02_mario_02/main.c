c59ced747f1c4d65a0fe461608bdb4016e4563c9
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2022-10-01 15:17:13 -0300

    Add files via upload

---
+++ 02_mario_02/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void mauro(int height);
+
+int main() {
+    int height = 0;
+    printf("Enter valor height:  ");
+    scanf("%d", &height);
+    mauro(height);
+    
+    return 0;
+}
+
+void mauro(int height) {
+    int width = 1;
+    //Width would be #
+    int void_spaces = height-1;
+    //void_spaces would be the void spaces
+    for (int i = 0; i < height; ++i) {
+        for (int space = 0; space < void_spaces; ++space) {
+            printf(" ");
+        }
+        for (int x = 0; x < width; ++x) {
+            printf("#");
+        }
+        printf(" ");
+        //space between first stair and second stair
+        for(int y = 0; y < width; ++y) {
+          printf("#");
+          //add second stair
+        }
+        ++width;
+        //add more #
+        --void_spaces;
+        //decrease empty spaces
+        printf("\n");
+    }
+}
