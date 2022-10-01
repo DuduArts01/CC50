50ea104d6af63bde076b089738c0914838f01620
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2022-10-01 15:08:16 -0300

    Add files via upload

--- 01_mario_01/mario.c
+++ 01_mario_01/mario.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 
-void stair(int h);
+void mauro(int height);
 
-int main(void) {
-  int height;
-  printf("Enter valor of height: ");
-  scanf("%i", &height);
-  stair(height);
-  return 0;
+int main() {
+    int height = 0;
+    printf("Enter valor height:  ");
+    scanf("%d", &height);
+    mauro(height);
+    
+    return 0;
 }
 
-void stair(int h) {  
-  int width = h - 1;
-  int c, c2;
-    for(int i = 0; i < h; ++i) {
-      for(int j = 0; j < width; ++j) {
-        printf(" ");
-        c = j;
-      } 
-      if(c == width){
-        for(int j = c; j < h - 1; ++j) {
-          printf("#");
-          c2 = j;
+void mauro(int height) {
+    int width = 1;
+    //Width would be #
+    int void_spaces = height-1;
+    //void_spaces would be the void spaces
+    for (int i = 0; i < height; ++i) {
+        for (int space = 0; space < void_spaces; ++space) {
+            printf(" ");
         }
-      } 
-      if(c2 == h - 1){
+        for (int x = 0; x < width; ++x) {
+            printf("#");
+        }
+        ++width;
+        //add more #
+        --void_spaces;
+        //decrease empty spaces
         printf("\n");
-      }
-      --width;
-      
     }
 }
