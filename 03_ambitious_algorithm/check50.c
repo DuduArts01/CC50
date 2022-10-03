d6ce8a40c899ccd40f34298daed44325b4202949
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2022-10-02 22:57:42 -0300

    Add files via upload

--- 03_ambitious_algorithm/check50.c
+++ 03_ambitious_algorithm/check50.c
@@ -1,49 +1,51 @@
-#include <stdio.h>
-#include <math.h>
-
-int check_coins(float c);
-
-int main(void) {
-  float dolar;
-
-  printf("Troca devida: ");
-  scanf("%f", &dolar);  
-  int change_coins = round(dolar * 100);
-  if(change_coins < 0){
-    printf("Troca devida: foo\n");
-    printf("Troca devida: %.2f\n", change_coins * -1);
-    printf("%d coin.\n", check_coins(change_coins * -1));
-    //check change coins
-  } else {    
-    printf("%d coin.\n", check_coins(change_coins));
-    //check change coins
-  }
-    
-  return 0;
-}
-
-int check_coins(float c) {  
-  int x = 0, coin = 0, co = c;  
+#include <stdio.h> 
+ #include <math.h> 
+  
+ int check_coins(int c); 
+  
+ int main(void) { 
+   float dolar; 
+  
+   printf("Troca devida: "); 
+   scanf("%f", &dolar);   
+   int change_coins = round(dolar * 100); 
+   //round() --> round off the float to the maximum
    
-  while(co > 0) {
-    if(co >= 25){      
-      co -= 25;
-      coin += 1;      
-      
-    } else if(co >= 10) {
-      co -= 10;
-      coin += 1;
-      
-    } else if(co >= 5) {
-      co -= 5;
-      coin += 1;
-      
-    } else {
-      co -= 1;
-      coin += 1;      
-      
-    }
-  return coin;
-
-  }
-}
+   if(change_coins < 0){ 
+     printf("Troca devida: foo\n"); 
+     printf("Troca devida: %.2f\n", dolar * -1); 
+     printf("%d coin.\n", check_coins(change_coins * -1)); 
+     //check change coins and if it is negative
+   } else {     
+     printf("%d coin.\n", check_coins(change_coins)); 
+     //check change coins and if it is positive
+   } 
+      
+   return 0; 
+ } 
+  
+ int check_coins(int c) {   
+   int coin = 0; 
+     
+   while(c > 0) { 
+     if(c >= 25){       
+       c -= 25; 
+       coin += 1;       
+       
+     } else if(c >= 10) { 
+       c -= 10; 
+       coin += 1; 
+        
+     } else if(c >= 5) { 
+       c -= 5; 
+       coin += 1; 
+        
+     } else { 
+       c -= 1; 
+       coin += 1;       
+        
+     } 
+   
+  } 
+ return coin;
+ }
