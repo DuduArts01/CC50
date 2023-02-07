408660cd8b2442e45fdfd9bf6a0a08126b663ee5
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2023-02-07 13:20:18 -0300

    Update credit.c

--- 04_credit/credit.c
+++ 04_credit/credit.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main() {
+    int number, i = 0, c = 0;
+    int digits[9999];
+    
+    printf("Number: ");
+    scanf("%i", &number);
+    
+    while(number > 0){
+        digits[i] = (number % 10);
+        number = number / 10;
+        i++;
+    }   
+    
+    while(c < i){
+        printf("%i ", digits[c]);
+        c++;
+        
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
